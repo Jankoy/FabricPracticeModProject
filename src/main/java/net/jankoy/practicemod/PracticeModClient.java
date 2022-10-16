4e2d3a66096b27f490790d0f40d2b101da8887d8
Jankoy <jacob121.jb@gmail.com>
2022-10-16 12:44:52 -0400

    First Commit!

---
+++ src/main/java/net/jankoy/practicemod/PracticeModClient.java
@@ -0,0 +1,11 @@
+package net.jankoy.practicemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PracticeModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
